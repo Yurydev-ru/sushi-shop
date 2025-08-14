76ca7923abc6b5581add16f49481fcd059311179
Yurydevru <yurydevru@gmail.com>
2025-08-14 22:50:15 +0200

    added-first-styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['~/assets/styles/main.scss'],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
