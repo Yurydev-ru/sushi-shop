ed65f6100c081bc1c137b4edc5c15d2443f31978
Yurydevru <yurydevru@gmail.com>
2025-08-14 21:33:14 +0200

    add home pages

---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the main content of the home page.</p>
+        <p>Feel free to explore the site!</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
